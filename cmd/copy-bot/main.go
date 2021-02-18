9157103eb2d70b0940324f89c7c4fa5e15999122
Philipp <boeschenphilipp@gmail.com>
2021-02-18 19:07:21 +0100

    Pull listen addr from env

--- cmd/copy-bot/main.go
+++ cmd/copy-bot/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Deichindianer/copy-bot/internal/copybot"
 )
 
 func main() {
 	cp := copybot.NewCopyBot()
-	log.Fatal(http.ListenAndServe(":8080", cp))
+	if os.Getenv("COPYBOT_LISTEN_ADDR") == "" {
+		log.Fatal("Set COPYBOT_LISTEN_ADDR to a proper listen address!")
+	}
+	log.Fatal(http.ListenAndServe(os.Getenv("COPYBOT_LISTEN_ADDR"), cp))
 }
